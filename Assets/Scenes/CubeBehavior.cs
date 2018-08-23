8aa94eb98c081f7a983fed3045c1b0e9fbd7873b
Tom Richards <tom@tomrichards.net>
2018-08-22 23:36:41 -0400

    Add movement

--- Assets/Scenes/CubeBehavior.cs
+++ Assets/Scenes/CubeBehavior.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("I am alive!");
-        Debug.Log("I am alive!");
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
         Debug.Log("I am alive!");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update () {
+        float horizontalDistance = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalDistance);
+
+        float verticalDistance = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * verticalDistance);
+    }
 }
