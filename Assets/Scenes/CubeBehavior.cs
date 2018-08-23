ae51407520f6a0bcc3952c0942446382c7325f63
Tom Richards <tom@tomrichards.net>
2018-08-22 23:04:15 -0400

    Add cube behavior

---
+++ Assets/Scenes/CubeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I am alive!");
+        Debug.Log("I am alive!");
+        Debug.Log("I am alive!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
